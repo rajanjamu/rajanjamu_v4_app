4fd12c1f08802fabd9e17c5e184f03b0014d3ac6
Rajan Gupta <rajanjamu@gmail.com>
2015-01-07 20:48:34 +0530

    modified home to add qoute

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'projects' => 'static_pages/projects'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
